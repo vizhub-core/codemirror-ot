590d2ac1a0e268350bbcb07ce3d804741d98718f
curran <curran.kelleher@gmail.com>
2018-10-28 02:22:36 +0530

    Make opsToTransaction work with string replacement. Closes #9

--- src/index.ts
+++ src/index.ts
@@ -4,14 +4,12 @@ export type Path = (number | string)[];
 
 interface Op {
   p: Path
-}
-interface StringOp extends Op {
   si?: string; // string insert
   sd?: string; // string delete
 }
 
 const changeToOps = (path: Path, transaction: Transaction) => (change: Change) => {
-  const ops: StringOp[] = [];
+  const ops: Op[] = [];
   const p = path.concat([change.from]);
   if (change.from !== change.to) {
     ops.push({ p, sd: transaction.startState.doc.slice(change.from, change.to) });
@@ -28,18 +26,17 @@ export const transactionToOps = (path: Path, transaction: Transaction) =>
     .reduce((accumulator, ops) => accumulator.concat(ops), []);
 
 const opToChange = (transaction: Transaction, op: Op) => {
-  const stringOp = op as StringOp;
-  const from = stringOp.p[op.p.length - 1] as number;
+  const from = op.p[op.p.length - 1] as number;
 
   // String insert
-  if (stringOp.si !== undefined) {
-    const str = stringOp.si;
+  if (op.si !== undefined) {
+    const str = op.si;
     return transaction.change(new Change(from, from, [str]));
   }
 
   // String delete
-  if (stringOp.sd !== undefined) {
-    const to = from + stringOp.sd.length;
+  if (op.sd !== undefined) {
+    const to = from + op.sd.length;
     const str = '';
     return transaction.change(new Change(from, to, [str]));
   }
@@ -47,7 +44,23 @@ const opToChange = (transaction: Transaction, op: Op) => {
   throw new Error('Invalid string op.');
 }
 
+const eq = (a, b) => a.length === b.length && a.every((aItem, i) => aItem === b[i]);
+const isReplacement = ops => ops.length === 2 && eq(ops[0].p, ops[1].p) && ops[0].sd && ops[1].si;
+
+const replacementOpsToChange = (transaction: Transaction, ops: Op[]) => {
+  const op0 = ops[0];
+  const sd = op0.sd;
+  const si = ops[1].si;
+
+  const from = op0.p[op0.p.length - 1] as number;
+  const to = from + sd.length;
+
+  return transaction.change(new Change(from, to, [si]));
+}
+
 export const opsToTransaction = (path: Path, state: EditorState, ops: Op[]) =>
-  ops.reduce(opToChange, state.transaction);
+  isReplacement(ops)
+    ? replacementOpsToChange(state.transaction, ops)
+    : ops.reduce(opToChange, state.transaction);
 
 export { ot } from './ot';
