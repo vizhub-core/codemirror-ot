99a59920425453ea5555daf278cb961c869ea9e3
curran <curran.kelleher@gmail.com>
2018-10-27 13:45:45 +0530

    Got parts of original CodeMirror state tests to run!

--- test/test.ts
+++ test/test.ts
@@ -1,32 +1,48 @@
 import * as assert from 'assert';
-import { transactionToOps, opsToTransaction } from '../src/index';
+import { EditorState, Change } from 'codemirror-6';
+//import { transactionToOps, opsToTransaction } from '../src/index';
 
-describe('codemirror-ot', () => {
-  it('should insert a single character', () => {
-    const path = ['text'];
-    const transaction = {
-      changes: {
-        changes: [
-          {
-            from: 0,
-            to: 0,
-            length: 1,
-            text: ['d']
-          }
-        ]
-      }
-    };
-    const ops = [{
-      p: [ path, 0 ],
-      si: 'd'
-    }];
-    assert.deepEqual(
-      transactionToOps(path, transaction),
-      ops
-    ); 
-    assert.deepEqual(
-      opsToTransaction(path, ops),
-      transaction
-    ); 
-  });
+describe("EditorState", () => {
+  it("holds doc and selection properties", () => {
+    let state = EditorState.create({doc: "hello"})
+    assert.equal(state.doc.toString(), "hello")
+    assert.equal(state.selection.primary.from, 0)
+  })
+  
+  it("can apply changes", () => { 
+    let state = EditorState.create({doc: "hello"})
+    let transaction = state.transaction.change(new Change(2, 4, ["w"])).change(new Change(4, 4, ["!"]))
+    assert.equal(transaction.doc.toString(), "hewo!")
+    assert.equal(transaction.apply().doc.toString(), "hewo!")
+  })
 });
+
+//describe('codemirror-ot', () => {
+//  describe('single character insertion', () => {
+//    const path = ['text'];
+//    const transaction = {
+//      changes: {
+//        changes: [
+//          {
+//            from: 0,
+//            to: 0,
+//            length: 1,
+//            text: ['d']
+//          }
+//        ]
+//      }
+//    };
+//    const ops = [{
+//      p: [ path, 0 ],
+//      si: 'd'
+//    }];
+//
+//    it('transactionToOps', () => {
+//      assert.deepEqual(transactionToOps(path, transaction), ops); 
+//    });
+//
+//    it('opsToTransaction', () => {
+//      assert.deepEqual(opsToTransaction(path, ops), transaction); 
+//    });
+//  });
+//});
