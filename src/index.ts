2afc027d5481b163d0474c8b8fe3eed72a2e81c1
curran <curran.kelleher@gmail.com>
2018-10-27 15:29:17 +0530

    Got first real test to pass.

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
-import { Change, Transaction } from 'codemirror-6';
-type Path = (number | string)[];
+import { Change, Transaction, EditorState } from 'codemirror-6';
+
+export type Path = (number | string)[];
 
 interface Op { p: Path }
 interface StringInsert extends Op { si: string }
@@ -14,23 +15,14 @@ const changeToOp = (path: Path) => (change: Change): StringOp => ({
 export const transactionToOps = (path: Path, transaction: Transaction) =>
   transaction.changes.changes.map(changeToOp(path));
 
-const opToChange = (path: Path) => (op: Op) => {
+const opToChange = (transaction: Transaction, op: Op) => {
   const stringInsert = op as StringInsert;
-  const from = stringInsert.p[op.p.length - 1];
+  const from = stringInsert.p[op.p.length - 1] as number;
   const str = stringInsert.si;
+  return transaction.change(new Change(from, from, [str]));
+}
 
-  return {
-    from: from,
-    to: from,
-    length: str.length,
-    text: [ str ]
-  };
-};
-
-export const opsToTransaction = (path: Path, ops: Op[]) => ({
-  changes: {
-    changes: ops.map(opToChange(path))
-  }
-});
+export const opsToTransaction = (path: Path, state: EditorState, ops: Op[]) =>
+  ops.reduce(opToChange , state.transaction);
 
 export { ot } from './ot';
--- test/test.ts
+++ test/test.ts
@@ -1,48 +1,39 @@
 import * as assert from 'assert';
-import { EditorState, Change } from 'codemirror-6';
-//import { transactionToOps, opsToTransaction } from '../src/index';
+import { EditorState, Change, Transaction } from 'codemirror-6';
+import {
+  transactionToOps,
+  opsToTransaction,
+  Path
+} from '../src/index';
 
-describe("EditorState", () => {
-  it("holds doc and selection properties", () => {
-    let state = EditorState.create({doc: "hello"})
-    assert.equal(state.doc.toString(), "hello")
-    assert.equal(state.selection.primary.from, 0)
-  })
-  
-  it("can apply changes", () => { 
-    let state = EditorState.create({doc: "hello"})
-    let transaction = state.transaction.change(new Change(2, 4, ["w"])).change(new Change(4, 4, ["!"]))
-    assert.equal(transaction.doc.toString(), "hewo!")
-    assert.equal(transaction.apply().doc.toString(), "hewo!")
-  })
+// Removes meta.time, which is the only thing that doesn't match.
+const withoutTimestamp = (transaction: Transaction) => {
+  // Hack around TypeScript's complaint that meta is private.
+  const object = JSON.parse(JSON.stringify(transaction));
+  delete object.meta.time;
+  return object;
+};
+
+describe('codemirror-ot', () => {
+  const path: Path = ['text'];
+  let state = EditorState.create({ doc: 'hello' });
+  describe('single character insertion', () => {
+    const transaction = state.transaction.change(new Change(0, 0, ['d']));
+
+    const ops = [{
+      p: path.concat([0]),
+      si: 'd'
+    }];
+
+    it('transactionToOps', () => {
+      assert.deepEqual(transactionToOps(path, transaction), ops); 
+    });
+
+    it('opsToTransaction', () => {
+      assert.deepEqual(
+        withoutTimestamp(opsToTransaction(path, state, ops)),
+        withoutTimestamp(transaction)
+      ); 
+    });
+  });
 });
-
-//describe('codemirror-ot', () => {
-//  describe('single character insertion', () => {
-//    const path = ['text'];
-//    const transaction = {
-//      changes: {
-//        changes: [
-//          {
-//            from: 0,
-//            to: 0,
-//            length: 1,
-//            text: ['d']
-//          }
-//        ]
-//      }
-//    };
-//    const ops = [{
-//      p: [ path, 0 ],
-//      si: 'd'
-//    }];
-//
-//    it('transactionToOps', () => {
-//      assert.deepEqual(transactionToOps(path, transaction), ops); 
-//    });
-//
-//    it('opsToTransaction', () => {
-//      assert.deepEqual(opsToTransaction(path, ops), transaction); 
-//    });
-//  });
-//});
