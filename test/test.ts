6f77ad5f718c7d58f956f44507d4619afb99f856
curran <curran.kelleher@gmail.com>
2018-10-27 18:45:47 +0530

    Import json0

--- test/test.ts
+++ test/test.ts
@@ -1,10 +1,7 @@
 import * as assert from 'assert';
 import { EditorState, Change, Transaction } from 'codemirror-6';
-import {
-  transactionToOps,
-  opsToTransaction,
-  Path
-} from '../src/index';
+import * as json0 from 'ot-json0';
+import { transactionToOps, opsToTransaction, Path } from '../src/index';
 
 // Removes meta.time, which is the only thing that doesn't match.
 const withoutTimestamp = (transaction: Transaction) => {
@@ -14,16 +11,16 @@ const withoutTimestamp = (transaction: Transaction) => {
   return object;
 };
 
+console.log(json0);
+
+
 describe('codemirror-ot', () => {
   const path: Path = ['text'];
   let state = EditorState.create({ doc: 'hello' });
+
   describe('single character insertion', () => {
     const transaction = state.transaction.change(new Change(0, 0, ['d']));
-
-    const ops = [{
-      p: path.concat([0]),
-      si: 'd'
-    }];
+    const ops = [{ p: path.concat([0]), si: 'd' }];
 
     it('transactionToOps', () => {
       assert.deepEqual(transactionToOps(path, transaction), ops); 
