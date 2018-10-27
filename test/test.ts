c9a9a14c776905808c2ccac0e678e64de0720e4d
curran <curran.kelleher@gmail.com>
2018-10-27 19:09:33 +0530

    Refactor generic verify function

--- test/test.ts
+++ test/test.ts
@@ -11,37 +11,38 @@ const withoutTimestamp = (transaction: Transaction) => {
   return object;
 };
 
-describe('codemirror-ot', () => {
+const verify = ({ textBefore, textAfter, txn, ops }) => {
   const path: Path = [];
+  const state = EditorState.create({ doc: textBefore });
+  const transaction = txn(state.transaction);
 
+  it('transactionToOps', () => {
+    assert.deepEqual(transactionToOps(path, transaction), ops); 
+  });
+
+  it('opsToTransaction', () => {
+    assert.deepEqual(
+      withoutTimestamp(opsToTransaction(path, state, ops)),
+      withoutTimestamp(transaction)
+    ); 
+  });
+
+  it('applied ops should match expected text', () => {
+    assert.deepEqual(textAfter, json0.apply('', ops));
+  });
+
+  it('applied transaction should match expected text', () => {
+    assert.deepEqual(textAfter, transaction.doc.toString());
+  });
+};
+
+describe('codemirror-ot', () => {
   describe('single character insertion', () => {
-
-    let textBefore = '';
-    let textAfter = 'd';
-
-    const state = EditorState.create({ doc: textBefore });
-
-    const transaction = state.transaction.change(new Change(0, 0, ['d']));
-    const ops = [{ p: path.concat([0]), si: 'd' }];
-
-    it('transactionToOps', () => {
-      assert.deepEqual(transactionToOps(path, transaction), ops); 
+    verify({
+      textBefore: '',
+      textAfter:'d',
+      txn: transaction => transaction.change(new Change(0, 0, ['d'])),
+      ops: [{ p: [0], si: 'd' }]
     });
-
-    it('opsToTransaction', () => {
-      assert.deepEqual(
-        withoutTimestamp(opsToTransaction(path, state, ops)),
-        withoutTimestamp(transaction)
-      ); 
-    });
-
-    it('applied ops should match expected text', () => {
-      assert.deepEqual(textAfter, json0.apply('', ops));
-    });
-
-    it('applied transaction should match expected text', () => {
-      assert.deepEqual(textAfter, transaction.doc.toString());
-    });
-
   });
 });
