b09333c5560ff029b09a7f9e01534e3be7f38a8b
curran <curran.kelleher@gmail.com>
2018-10-28 16:30:34 +0530

    Sketch of ideas for plugin API

--- src/ot.ts
+++ src/ot.ts
@@ -4,23 +4,39 @@ import {
   EditorState,
   Transaction
 } from 'codemirror-6';
+import { opsToTransaction } from './opsToTransaction';
+import { transactionToOps } from './transactionToOps';
 
-export const ot = new Plugin({
-  view: (view: EditorView) => {
+export const ot = emitOps => {
+  let plugin;
+  const dispatchOp = new Promise(resolve => {
+    console.log('A');
+    plugin = new Plugin({
+      view: (view: EditorView) => {
 
-    // Inject programmatically created transactions,
-    // from remote OT operations.
-    setTimeout(() => {
-      console.log('dispatching OT');
-      view.dispatch(view.state.transaction.replace(1, 2, "*"))
-    }, 2000);
+        // Inject programmatically created transactions,
+        // from remote OT operations.
+        resolve(ops => {
+          console.log('dispatching OT');
+          view.dispatch(opsToTransaction(ops));
+        });
 
-    return {
-      // Listen for all transactions,
-      // so they can be converted to OT operations.
-      updateState(view: EditorView, prev: EditorState, trs: Transaction[]) {
-        console.log(trs);
+        return {
+          // Listen for all transactions,
+          // so they can be converted to OT operations.
+          updateState(view: EditorView, prev: EditorState, transactions: Transaction[]) {
+            const ops = transactions.reduce((accumulator, transaction) => {
+              return accumulator.concat(transactionToOps(transaction));
+            }, []);
+            emitOps(ops);
+          }
+        };
       }
-    };
-  }
-});
+    });
+  });
+  console.log('B');
+  return {
+    plugin,
+    dispatchOp
+  };
+};
