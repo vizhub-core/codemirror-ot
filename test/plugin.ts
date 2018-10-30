d27fbc8ce8aff77bca0994f9722bbdde82d08ed6
curran <curran.kelleher@gmail.com>
2018-10-31 01:02:48 +0530

    Add passing test for plugin implementation. Closes #12

--- test/plugin.ts
+++ test/plugin.ts
@@ -1,5 +1,5 @@
 import * as assert from 'assert';
-import { EditorState } from 'codemirror-6';
+import { EditorState, Change } from 'codemirror-6';
 import { otPlugin, Path, Op } from '../src/index';
 
 describe('ot plugin', () => {
@@ -7,44 +7,14 @@ describe('ot plugin', () => {
     let emittedOps: Op[] = [];
     const path: Path = [];
     const emitOps = ops => emittedOps = ops;
-    const plugin = otPlugin(path, emitOps);
 
-    const doc = 'HelloWorld';
-    const plugins = [plugin];
+    const state = EditorState.create({
+      doc: 'HelloWorld',
+      plugins: [otPlugin(path, emitOps)]
+    });
 
-    // This line currently fails with error 'ReferenceError: document is not defined'
-    EditorState.create({ doc, plugins });
+    state.transaction.change(new Change(0, 0, ['d'])).apply();
 
-    assert.deepEqual(emittedOps, []);
-
-    //plugin.spec.view(view);
-
-    //const dispatchOps = await dispatchOpsPromise;
-
-    //const expectedOps = [];//[{ p: [5], si: ' ' }];
-    //assert.deepEqual(emittedOps, expectedOps);
-    //dispatchOps(expectedOps);
+    assert.deepEqual(emittedOps, [{ p: [0], si: 'd' }]);
   });
-  //it('should dispatch ops', async () => {
-    //let emittedOps = [];
-
-    //const path = [];
-    //const emitOps = ops => emittedOps = ops;
-    //const { plugin, dispatchOpsPromise } = ot({ path, emitOps });
-
-    //const doc = 'HelloWorld';
-    //const plugins = [plugin];
-    //const view = new EditorView(EditorState.create({ doc, plugins }))
-
-    //plugin.spec.view(view);
-
-    //const dispatchOps = await dispatchOpsPromise;
-
-    //const expectedOps = [];//[{ p: [5], si: ' ' }];
-    //assert.deepEqual(emittedOps, expectedOps);
-    //dispatchOps(expectedOps);
-    //
-    //   after: 'Hello World',
-    //   txn: transaction => transaction.change(new Change(5, 5, [' '])),
-  //});
 });
