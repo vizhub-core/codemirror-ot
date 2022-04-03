33880bbb2acaf7f9b7db580ea1e170dabb30601c
Curran <curran.kelleher@gmail.com>
2022-04-03 00:00:43 -0400

    Got all translation tests to pass.

--- test/test.js
+++ test/test.js
@@ -1,93 +1,9 @@
 import * as assert from 'assert';
-import { EditorState, ChangeSet } from '../src/codemirror';
-import json0 from 'ot-json0';
 import json1 from 'ot-json1';
 import textUnicode from 'ot-text-unicode';
-import otDiff from 'json0-ot-diff';
-import jsondiff from 'json0-ot-diff';
-import diffMatchPatch from 'diff-match-patch';
-import {
-  changesToOpJSON0,
-  changesToOpJSON1,
-  opToChangesJSON0,
-  opToChangesJSON1,
-} from '../src/index';
-
-// Removes metadata, which is the only thing that doesn't match.
-//const withoutTimestamp = (transaction) => {
-//  delete transaction.metadata;
-//  return transaction;
-//};
-
-const atPath = (obj, path) => path.reduce((d, key) => d[key], obj);
-const clone = (obj) => JSON.parse(JSON.stringify(obj));
-const diffJSON0 = (a, b) => jsondiff(a, b, diffMatchPatch);
-const diffJSON1 = (a, b) => jsondiff(a, b, diffMatchPatch, json1, textUnicode);
-
-// Verifies that ops and transaction match in terms of behavior,
-// and that the translation between ops and transaction holds in both directions.
-const verify = (options) => {
-  const { before, after, changes, opJSON0, opJSON1, path = [] } = options;
-
-  it('JSON0 op should match computed diff', () => {
-    assert.deepEqual(opJSON0, diffJSON0(before, after));
-  });
-
-  it('JSON1 op should match computed diff', () => {
-    // console.log(JSON.stringify(opJSON1))
-    // console.log(JSON.stringify(diffJSON1(before, after)));
-    assert.deepEqual(opJSON1, diffJSON1(before, after));
-  });
-
-  it('JSON0 applied op should match expected text', () => {
-    assert.deepEqual(after, json0.type.apply(clone(before), opJSON0));
-  });
-
-  it('JSON0 inverted applied op should match expected text', () => {
-    assert.deepEqual(
-      before,
-      json0.type.apply(clone(after), json0.type.invert(opJSON0))
-    );
-  });
-
-  it('JSON1 applied op should match expected text', () => {
-    assert.deepEqual(after, json1.type.apply(clone(before), opJSON1));
-  });
-
-  it('JSON1 inverted applied op should match expected text', () => {
-    const opJSON1Invertible = json1.type.makeInvertible(opJSON1, before);
-    assert.deepEqual(
-      before,
-      json1.type.apply(clone(after), json1.type.invert(opJSON1Invertible))
-    );
-  });
-
-  const state = EditorState.create({ doc: atPath(before, path) });
-
-  it('applied transaction should match expected text', () => {
-    const expected = atPath(after, path);
-    const actual = state.update({ changes }).state.doc.sliceString(0);
-    assert.equal(expected, actual);
-  });
-
-  const changeSet = ChangeSet.of(changes, before.length);
-
-  it('changesToOpJSON0', () => {
-    assert.deepEqual(changesToOpJSON0(path, changeSet, state.doc), opJSON0);
-  });
-
-  it('changesToOpJSON1', () => {
-    assert.deepEqual(changesToOpJSON1(path, changeSet, state.doc), opJSON1);
-  });
-
-  it('opToChangesJSON0', () => {
-    assert.deepEqual(opToChangesJSON0(opJSON0), changes);
-  });
-
-  it('opToChangesJSON1', () => {
-    assert.deepEqual(opToChangesJSON1(opJSON1), changes);
-  });
-};
+import { verify } from './verify';
+import { EditorState, ChangeDesc } from '../src/codemirror';
+import { json1Sync } from '../src/index';
 
 describe('translation (transactionToOps and opsToTransaction)', () => {
   describe('string insert', () => {
@@ -144,147 +60,206 @@ describe('translation (transactionToOps and opsToTransaction)', () => {
         changes: [{ from: 5, to: 6 }],
         opJSON0: [{ p: [5], sd: ' ' }],
         opJSON1: json1.editOp([], 'text-unicode', textUnicode.remove(5, ' ')),
-        txn: (transaction) => transaction.change(new ChangeDesc(5, 6, [''])),
       });
     });
-    //describe('multiple character delete mid-string', () => {
-    //  verify({
-    //    before: 'Hello Beautiful World',
-    //    after: 'HelloWorld',
-    //    txn: (transaction) => transaction.change(new ChangeDesc(5, 16, [''])),
-    //    ops: [{ p: [5], sd: ' Beautiful ' }],
-    //  });
-    //});
+    describe('multiple character delete mid-string', () => {
+      verify({
+        before: 'Hello Beautiful World',
+        after: 'HelloWorld',
+        changes: [{ from: 5, to: 16 }],
+        opJSON0: [{ p: [5], sd: ' Beautiful ' }],
+        opJSON1: json1.editOp(
+          [],
+          'text-unicode',
+          textUnicode.remove(5, ' Beautiful ')
+        ),
+      });
+    });
+    describe('only character delete', () => {
+      verify({
+        before: 'd',
+        after: '',
+        changes: [{ from: 0, to: 1 }],
+        opJSON0: [{ p: [0], sd: 'd' }],
+        opJSON1: json1.editOp([], 'text-unicode', textUnicode.remove(0, 'd')),
+      });
+    });
   });
-  //describe('string delete and insert (replacement)', () => {
-  //  describe('single character replacement mid-string', () => {
-  //    verify({
-  //      before: 'Hello World',
-  //      after: 'Hello-World',
-  //      txn: (transaction) => transaction.change(new ChangeDesc(5, 6, ['-'])),
-  //      ops: [
-  //        { p: [5], sd: ' ' },
-  //        { p: [5], si: '-' },
-  //      ],
-  //      txnFromOps: (transaction) =>
-  //        transaction
-  //          .change(new ChangeDesc(5, 6, ['']))
-  //          .change(new ChangeDesc(5, 5, ['-'])),
-  //    });
-  //  });
+  describe('string replacement', () => {
+    describe('single character replacement mid-string', () => {
+      // Sanity check
+      assert.deepEqual(
+        json1.editOp(
+          [],
+          'text-unicode',
+          textUnicode.type.compose(
+            textUnicode.remove(5, ' '),
+            textUnicode.insert(5, '-')
+          )
+        ),
+        [{ es: [5, '-', { d: ' ' }] }]
+      );
+
+      verify({
+        before: 'Hello World',
+        after: 'Hello-World',
+        changes: [{ from: 5, to: 6, insert: '-' }],
+        // Note that the above changes are functionally equivalent to:
+        //changes: [
+        //  { from: 5, to: 6 },
+        //  { from: 5, to: 5, insert: '-' },
+        //],
+        opJSON0: [
+          { p: [5], sd: ' ' },
+          { p: [5], si: '-' },
+        ],
+        opJSON1: json1.editOp(
+          [],
+          'text-unicode',
+          textUnicode.type.compose(
+            textUnicode.remove(5, ' '),
+            textUnicode.insert(5, '-')
+          )
+        ),
+      });
+    });
+  });
+  describe('newlines', () => {
+    describe('newline insert', () => {
+      verify({
+        before: '',
+        after: '\n',
+        changes: [{ from: 0, to: 0, insert: '\n' }],
+        opJSON0: [{ p: [0], si: '\n' }],
+        opJSON1: json1.editOp([], 'text-unicode', textUnicode.insert(0, '\n')),
+        //before: '',
+        //after: '\n',
+        //txn: (transaction) => transaction.change(new ChangeDesc(0, 0, ['', ''])),
+        //ops: [{ p: [0], si: '\n' }],
+      });
+    });
+    describe('newline delete', () => {
+      verify({
+        before: '\n',
+        after: '',
+        changes: [{ from: 0, to: 1 }],
+        opJSON0: [{ p: [0], sd: '\n' }],
+        opJSON1: json1.editOp([], 'text-unicode', textUnicode.remove(0, '\n')),
+      });
+      //  before: '\n',
+      //  after: '',
+      //  txn: (transaction) => transaction.change(new ChangeDesc(0, 1, [''])),
+      //  ops: [{ p: [0], sd: '\n' }],
+    });
+    describe('replace with newlines', () => {
+      verify({
+        before: 'eat\na\npie',
+        after: 'eat\nthe\napple\npie',
+        changes: [{ from: 4, to: 5, insert: 'the\napple' }],
+        opJSON0: [
+          { sd: 'a', p: [4] },
+          { si: 'the\napple', p: [4] },
+        ],
+        opJSON1: json1.editOp(
+          [],
+          'text-unicode',
+          textUnicode.type.compose(
+            textUnicode.remove(4, 'a'),
+            textUnicode.insert(4, 'the\napple')
+          )
+        ),
+      });
+    });
+  });
+  describe('paths', () => {
+    describe('multiple character insert mid-string', () => {
+      verify({
+        path: ['title'],
+        before: { title: 'HelloWorld' },
+        after: { title: 'Hello Beautiful World' },
+        opJSON0: [{ si: ' Beautiful ', p: ['title', 5] }],
+        opJSON1: ['title', { es: [5, ' Beautiful '] }],
+        changes: [{ from: 5, to: 5, insert: ' Beautiful ' }],
+      });
+    });
+    describe('multiple character delete mid-string', () => {
+      verify({
+        path: ['title'],
+        before: { title: 'Hello Beautiful World' },
+        after: { title: 'HelloWorld' },
+        opJSON0: [{ sd: ' Beautiful ', p: ['title', 5] }],
+        opJSON1: ['title', { es: [5, { d: ' Beautiful ' }] }],
+        changes: [{ from: 5, to: 16 }],
+      });
+    });
+    describe('replace with newlines', () => {
+      verify({
+        path: ['title'],
+        before: { title: 'eat\na\npie' },
+        after: { title: 'eat\nthe\napple\npie' },
+        opJSON0: [
+          { sd: 'a', p: ['title', 4] },
+          { si: 'the\napple', p: ['title', 4] },
+        ],
+        opJSON1: ['title', { es: [4, 'the\napple', { d: 'a' }] }],
+        changes: [{ from: 4, to: 5, insert: 'the\napple' }],
+      });
+    });
+    describe('deep paths', () => {
+      verify({
+        path: ['files', 'README.md'],
+        before: { files: { 'README.md': 'HelloWorld' } },
+        after: { files: { 'README.md': 'Hello Beautiful World' } },
+        opJSON0: [{ si: ' Beautiful ', p: ['files', 'README.md', 5] }],
+        opJSON1: ['files', 'README.md', { es: [5, ' Beautiful '] }],
+        changes: [{ from: 5, to: 5, insert: ' Beautiful ' }],
+      });
+    });
+  });
+});
+// TODO consider porting the y-codemirror tests into here.
+// They do interesting things with randomness.
+// They also cover the case of a CodeMirror change
+// containing multiple changes
+
+// TODO test this level
+describe('ot CodeMirror Extension (json1Sync)', () => {
+  const getAtPath = ({ shareDBDoc, path }) =>
+    path.reduce((accumulator, key) => accumulator[key], shareDBDoc.data);
+
+  const createCodeMirrorEditor = ({ shareDBDoc, path }) =>
+    new EditorView({
+      state: EditorState.create({
+        doc: getAtPath({ shareDBDoc, path }),
+        extensions: [json1Sync({ shareDBDoc, path })],
+      }),
+    });
+
+  // TODO upgrade these ancient tests to the latest CodeMirror API
+  //
+  //let emittedOps = [];
+  //const path = [];
+  //const emitOp = (ops) => (emittedOps = ops);
+
+  //const state = EditorState.create({
+  //  doc: 'HelloWorld',
+  //  extensions: [ot(path, emitOp)],
   //});
-  //describe('newlines', () => {
-  //  describe('newline insert', () => {
-  //    verify({
-  //      before: '',
-  //      after: '\n',
-  //      txn: (transaction) => transaction.change(new ChangeDesc(0, 0, ['', ''])),
-  //      ops: [{ p: [0], si: '\n' }],
-  //    });
-  //  });
-  //  describe('newline delete', () => {
-  //    verify({
-  //      before: '\n',
-  //      after: '',
-  //      txn: (transaction) => transaction.change(new ChangeDesc(0, 1, [''])),
-  //      ops: [{ p: [0], sd: '\n' }],
-  //    });
-  //  });
-  //  describe('replace with newlines', () => {
-  //    verify({
-  //      before: 'eat\na\npie',
-  //      after: 'eat\nan\napple\npie',
-  //      txn: (transaction) =>
-  //        transaction.change(new ChangeDesc(4, 5, ['an', 'apple'])),
-  //      ops: [
-  //        { p: [4], sd: 'a' },
-  //        { p: [4], si: 'an\napple' },
-  //      ],
-  //      txnFromOps: (transaction) =>
-  //        transaction
-  //          .change(new ChangeDesc(4, 5, ['']))
-  //          .change(new ChangeDesc(4, 4, ['an', 'apple'])),
-  //    });
-  //  });
+
+  //it('should emit ops', async () => {
+  //  state
+  //    .t()
+  //    .change(new ChangeDesc(0, 0, ['d']))
+  //    .apply();
+  //  assert.deepEqual(emittedOps, [{ p: [0], si: 'd' }]);
   //});
-  //describe('paths', () => {
-  //  describe('multiple character insert mid-string', () => {
-  //    verify({
-  //      path: ['title'],
-  //      before: { title: 'HelloWorld' },
-  //      after: { title: 'Hello Beautiful World' },
-  //      txn: (transaction) =>
-  //        transaction.change(new ChangeDesc(5, 5, [' Beautiful '])),
-  //      ops: [{ p: ['title', 5], si: ' Beautiful ' }],
-  //    });
-  //  });
-  //  describe('multiple character delete mid-string', () => {
-  //    verify({
-  //      path: ['title'],
-  //      before: { title: 'Hello Beautiful World' },
-  //      after: { title: 'HelloWorld' },
-  //      txn: (transaction) => transaction.change(new ChangeDesc(5, 16, [''])),
-  //      ops: [{ p: ['title', 5], sd: ' Beautiful ' }],
-  //    });
-  //  });
-  //  describe('replace with newlines', () => {
-  //    verify({
-  //      path: ['title'],
-  //      before: { title: 'eat\na\npie' },
-  //      after: { title: 'eat\nan\napple\npie' },
-  //      txn: (transaction) =>
-  //        transaction.change(new ChangeDesc(4, 5, ['an', 'apple'])),
-  //      ops: [
-  //        { p: ['title', 4], sd: 'a' },
-  //        { p: ['title', 4], si: 'an\napple' },
-  //      ],
-  //      txnFromOps: (transaction) =>
-  //        transaction
-  //          .change(new ChangeDesc(4, 5, ['']))
-  //          .change(new ChangeDesc(4, 4, ['an', 'apple'])),
-  //    });
-  //  });
-  //  describe('deep paths', () => {
-  //    verify({
-  //      path: ['files', 'README.md'],
-  //      before: { files: { 'README.md': 'HelloWorld' } },
-  //      after: { files: { 'README.md': 'Hello Beautiful World' } },
-  //      txn: (transaction) =>
-  //        transaction.change(new ChangeDesc(5, 5, [' Beautiful '])),
-  //      ops: [{ p: ['files', 'README.md', 5], si: ' Beautiful ' }],
-  //    });
-  //  });
+
+  //it('should not emit degenerate ops', async () => {
+  //  emittedOps = null;
+  //  state
+  //    .t()
+  //    .change(new ChangeDesc(0, 0, ['']))
+  //    .apply();
+  //  assert.equal(emittedOps, null);
   //});
 });
-//import * as assert from 'assert';
-//import { EditorState, ChangeDesc } from '../src/codemirror';
-//import { ot } from '../src/index';
-//
-//describe('ot', () => {
-//  //console.log('here');
-//  //let emittedOps = [];
-//  //const path = [];
-//  //const emitOp = (ops) => (emittedOps = ops);
-//
-//  //const state = EditorState.create({
-//  //  doc: 'HelloWorld',
-//  //  extensions: [ot(path, emitOp)],
-//  //});
-//
-//  //it('should emit ops', async () => {
-//  //  state
-//  //    .t()
-//  //    .change(new ChangeDesc(0, 0, ['d']))
-//  //    .apply();
-//  //  assert.deepEqual(emittedOps, [{ p: [0], si: 'd' }]);
-//  //});
-//
-//  //it('should not emit degenerate ops', async () => {
-//  //  emittedOps = null;
-//  //  state
-//  //    .t()
-//  //    .change(new ChangeDesc(0, 0, ['']))
-//  //    .apply();
-//  //  assert.equal(emittedOps, null);
-//  //});
-//});
