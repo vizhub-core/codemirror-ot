4c5e1eab3245760a98533ae87bbc47bb1364a8ea
Curran <curran.kelleher@gmail.com>
2022-04-03 10:15:30 -0400

    Bidirectional integration tests with mocked ShareDB

--- src/integration.js
+++ src/integration.js
@@ -1,26 +1,28 @@
 import { ViewPlugin } from '@codemirror/view';
 import { changesToOpJSON1, opToChangesJSON1 } from './translation';
 
-// Inspired by https://github.com/codemirror/collab/blob/cffa435ca5a7a0b3f46f59afdb45db4c9765b54e/src/collab.ts#L60
-// Inspired by https://codemirror.net/6/examples/collab/
-// Inspired by https://github.com/yjs/y-codemirror.next/blob/main/src/y-sync.js#L107
+// Inspired by:
+// https://github.com/codemirror/collab/blob/main/src/collab.ts
+// https://codemirror.net/6/examples/collab/
+// https://github.com/yjs/y-codemirror.next/blob/main/src/y-sync.js#L107
+// https://github.com/vizhub-core/vizhub/blob/main/vizhub-v2/packages/neoFrontend/src/pages/VizPage/Body/Editor/CodeEditor/CodeArea/CodeAreaCodeMirror5/index.js
 export const json1Sync = ({ shareDBDoc, path = [] }) =>
   ViewPlugin.fromClass(
     class {
-      // Add ShareDB listener.
+      // ShareDB --> CodeMirror
       constructor(view) {
         this.view = view;
+        this.handleOp = (op) => {
+          this.submittingOp = true;
+          view.dispatch({ changes: opToChangesJSON1(op) });
+          this.submittingOp = false;
+        };
         shareDBDoc.on('op', this.handleOp);
-      }
-
-      // ShareDB --> CodeMirror
-      handleOp(op) {
-        this.view.dispatch({ changes: opToChangesJSON1(op) });
       }
 
       // CodeMirror --> ShareDB
       update(update) {
-        if (update.docChanged) {
+        if (update.docChanged && !this.submittingOp) {
           shareDBDoc.submitOp(
             changesToOpJSON1(path, update.changes, this.view.state.doc)
           );
--- test/testIntegration.js
+++ test/testIntegration.js
@@ -1,7 +1,7 @@
 import * as assert from 'assert';
 import { json1Sync } from '../src/index';
-import { EditorState } from '@codemirror/state';
-import { EditorView } from '@codemirror/view';
+import { EditorState, ChangeSet } from '@codemirror/state';
+import { EditorView, ViewPlugin } from '@codemirror/view';
 import { JSDOM } from 'jsdom';
 
 // Gets a value at a path in a ShareDB Doc.
@@ -9,11 +9,11 @@ const getAtPath = (shareDBDoc, path) =>
   path.reduce((accumulator, key) => accumulator[key], shareDBDoc.data);
 
 // Creates a new CodeMirror EditorView with the json1Sync extension set up.
-const createEditor = ({ shareDBDoc, path }) =>
+const createEditor = ({ shareDBDoc, path, additionalExtensions = [] }) =>
   new EditorView({
     state: EditorState.create({
       doc: getAtPath(shareDBDoc, path),
-      extensions: [json1Sync({ shareDBDoc, path })],
+      extensions: [json1Sync({ shareDBDoc, path }), ...additionalExtensions],
     }),
   });
 
@@ -21,38 +21,24 @@ const createEditor = ({ shareDBDoc, path }) =>
 // Inspired by https://github.com/yjs/y-codemirror.next/blob/main/test/test.node.cjs
 
 const { window } = new JSDOM('');
-[
-  'window',
-  'innerWidth',
-  'innerHeight',
-  'document',
-  //'Node',
-  //'navigator',
-  //'Text',
-  //'HTMLElement',
-  'MutationObserver',
-].forEach((name) => {
-  global[name] = window[name];
-});
+['window', 'innerWidth', 'innerHeight', 'document', 'MutationObserver'].forEach(
+  (name) => {
+    global[name] = window[name];
+  }
+);
 
 global.requestAnimationFrame = (f) => setTimeout(f, 0);
 global.cancelAnimationFrame = () => {};
 
 export const testIntegration = () => {
-  describe('Mocked ShareDB Doc', () => {
-    it('CodeMirror --> ShareDB', (done) => {
+  describe('Mocked ShareDB', () => {
+    it('CodeMirror --> ShareDB', () => {
+      let submittedOp;
       const editor = createEditor({
         shareDBDoc: {
           data: { content: { files: { 2432: { text: 'Hello World' } } } },
           submitOp: (op) => {
-            assert.deepEqual(op, [
-              'content',
-              'files',
-              '2432',
-              'text',
-              { es: [5, '-', { d: '-' }] },
-            ]);
-            done();
+            submittedOp = op;
           },
           on: () => {},
         },
@@ -60,18 +46,67 @@ export const testIntegration = () => {
       });
 
       editor.dispatch({ changes: [{ from: 5, to: 6, insert: '-' }] });
+      assert.deepEqual(submittedOp, [
+        'content',
+        'files',
+        '2432',
+        'text',
+        { es: [5, '-', { d: '-' }] },
+      ]);
     });
-    // TODO it('ShareDB --> CodeMirror', (done) => {
-    //  const editor = createEditor({
-    //    shareDBDoc: {
-    //      data: { content: { files: { 2432: { text: 'Hello World' } } } },
-    //      submitOp: () => { },
-    //      on: () => {},
-    //    },
-    //    path: ['content', 'files', '2432', 'text'],
-    //  });
-    //  editor.listen for the event?
-    //});
+    it('ShareDB --> CodeMirror', () => {
+      let receiveOp;
+      let submittedOp;
+      let changes;
+      const text = 'Hello World';
+      const editor = createEditor({
+        shareDBDoc: {
+          data: { content: { files: { 2432: { text } } } },
+
+          // This handles ops submitted to ShareDB.
+          // We want to test that this does _not_ trigger
+          // for the case when our CodeMirror extension submits an op.
+          // Otherwise there would be an infinite loop.
+          submitOp: (op) => {
+            submittedOp = op;
+          },
+          // Here we mock the ShareDB Doc.on method.
+          // We can use `receiveOp` to simulate the ShareDB Doc
+          // receiving a remote op.
+          on: (eventName, callback) => {
+            if (eventName === 'op') {
+              receiveOp = callback;
+            }
+          },
+        },
+        path: ['content', 'files', '2432', 'text'],
+        additionalExtensions: [
+          ViewPlugin.fromClass(
+            class {
+              // Listen for changes to the CodeMirror editor view via this extension.
+              // Possibly a simpler way?
+              update(update) {
+                changes = update.changes;
+              }
+            }
+          ),
+        ],
+      });
+
+      // Simulate ShareDB receiving a remote op.
+      receiveOp([{ es: [5, '-', { d: '-' }] }]);
+
+      // verify that the remote op was translated to a CodeMirror change
+      // and dispatched to the editor view.
+      assert.deepEqual(
+        changes.toJSON(),
+        ChangeSet.of([{ from: 5, to: 6, insert: '-' }], text.length).toJSON()
+      );
+
+      // Verify that our extension did _not_ submit the receiced ShareDB op
+      // back into ShareDB.
+      assert.equal(submittedOp, undefined);
+    });
   });
 
   // TODO test for degenerate ops
