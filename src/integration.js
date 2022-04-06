b5a6f369ccd5d75240fffdd327c0e6253775d0a3
Curran <curran.kelleher@gmail.com>
2022-04-06 17:03:39 -0400

    Add module entry point, debug option

--- src/integration.js
+++ src/integration.js
@@ -6,17 +6,22 @@ import { changesToOpJSON1, opToChangesJSON1 } from './translation';
 // https://codemirror.net/6/examples/collab/
 // https://github.com/yjs/y-codemirror.next/blob/main/src/y-sync.js#L107
 // https://github.com/vizhub-core/vizhub/blob/main/vizhub-v2/packages/neoFrontend/src/pages/VizPage/Body/Editor/CodeEditor/CodeArea/CodeAreaCodeMirror5/index.js
-export const json1Sync = ({ shareDBDoc, path = [] }) =>
+export const json1Sync = ({ shareDBDoc, path = [], debug = false }) =>
   ViewPlugin.fromClass(
     class {
       // ShareDB --> CodeMirror
       constructor(view) {
         this.view = view;
         this.handleOp = (op) => {
-          if (!this.submittingOp) {
-            this.dispatching = true;
+          if (!this.lock) {
+            this.lock = true;
+            if (debug) {
+              console.log('Received op from ShareDB');
+              console.log('  ' + JSON.stringify(op));
+              console.log('  ' + JSON.stringify(opToChangesJSON1(op)));
+            }
             view.dispatch({ changes: opToChangesJSON1(op) });
-            this.dispatching = false;
+            this.lock = false;
           }
         };
         shareDBDoc.on('op', this.handleOp);
@@ -24,12 +29,22 @@ export const json1Sync = ({ shareDBDoc, path = [] }) =>
 
       // CodeMirror --> ShareDB
       update(update) {
-        if (update.docChanged && !this.dispatching) {
-          this.submittingOp = true;
+        if (update.docChanged && !this.lock) {
+          this.lock = true;
+          if (debug) {
+            console.log('Received change from CodeMirror');
+            console.log('  ' + JSON.stringify(update.changes));
+            console.log(
+              '  ' +
+                JSON.stringify(
+                  changesToOpJSON1(path, update.changes, this.view.state.doc)
+                )
+            );
+          }
           shareDBDoc.submitOp(
             changesToOpJSON1(path, update.changes, this.view.state.doc)
           );
-          this.submittingOp = false;
+          this.lock = false;
         }
       }
 
